1e53276af2645f9a6165019f72b51814cd26bfa4
Mike Simons <mike.simons@typeform.com>
2020-03-25 01:59:31 +0100

    Rewrite to support direct fetch from John Hopkins
    covid19api removed a bunch of endpoints so we'll skip 'em and go
    straight for the CSVs

--- covid.go
+++ covid.go
@@ -1,140 +1,242 @@
 package main
 
-import(
-	"os"
-	"log"
-	"io/ioutil"
+import (
+	"bufio"
 	"encoding/json"
 	"fmt"
-	"sort"
+	"github.com/hashicorp/go-getter"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 )
 
-type InputRecord struct {
+type Record struct {
+	Date time.Time
 	Country string
-	Province string
-	Status string
-	Cases int
-	Date string
+	Region string
 	Confirmed int
 	Deaths int
 	Recovered int
-}
-
-type OutputRecord struct {
-	Country string
-	Date string
-	Confirmed int
-	NewConfirmed int
-	Deaths int
-	NewDeaths int
-	Recovered int
-	NewRecovered int
-	Day int
 	Active int
+	Calculated map[string]int
 }
 
 func main() {
-	// Read data
-	var err error
-	f, err := os.Open(os.Args[1])
+	from, _ := time.Parse("2006-01-02", "2020-01-22")
+	to := time.Now().Add(-24 * time.Hour)
+
+	var records []Record
+	err := eachDay(from, to, func(d time.Time) error {
+		url := urlForDate(d)
+		file := fileForDate(d)
+
+		// Fetch file if we don't already have it
+		// Assumes that remote files are immutable
+		if _, err := os.Stat(file); os.IsNotExist(err) {
+			err := getter.GetFile(file, url)
+			if err != nil {
+				return fmt.Errorf("error fetching '%s': %w", url, err)
+			}
+		}
+
+		// Open data file for reading
+		fh, _ := os.Open(file)
+		defer fh.Close()
+		buf := bufio.NewReader(fh)
+
+		// Loop all rows
+		var mapper func(string)Record
+		for {
+			row, err := buf.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				return fmt.Errorf("error reading %s: %w", file, err)
+			}
+
+			// If we don't have a mapper already, try to create one
+			if mapper == nil {
+				mapper, err = rowMapper(row)
+				if err != nil {
+					log.Fatal(fmt.Errorf("unable to create row mapper: %w", err))
+				}
+			}
+
+			// Map row to record, augmenting with date
+			record := mapper(row)
+			record.Date = d
+			aggregate(&record)
+
+			records = append(records, record)
+		}
+
+		return nil
+	})
+
 	if err != nil {
-		log.Fatalf("Couldn't open file: %s", err)
+		log.Fatalf("unknown error: %s", err)
 	}
 
-	bytes, err := ioutil.ReadAll(f)
-	if err != nil {
-		log.Fatalf("Couldn't read file: %s", err)
+	for i, record := range records {
+		applyAggregates(&record)
+		fmt.Printf("{\"index\": {\"_id\": %d } }\n", i)
+		out, _ := json.Marshal(record)
+		fmt.Println(string(out))
 	}
+}
 
-	var data []InputRecord
-	err = json.Unmarshal(bytes, &data)
-	if err != nil {
-		log.Fatalf("Couldn't parse json: %s", err)
+// eachDay loops over days between the bounds and calls the callback function
+func eachDay(from time.Time, to time.Time, fn func(time.Time) error) error {
+	current := from
+	for ; !current.After(to); current = current.AddDate(0, 0, 1) {
+		err := fn(current)
+		if err != nil {
+			return err
+		}
 	}
+	return nil
+}
 
-	// Summarize data
-	countries := map[string]bool{}
-	dates := map[string]bool{}
-	counters := map[string]int{}
-	for _, entry := range data {
-		dkey := fmt.Sprintf("%s%s%s-day", entry.Country, entry.Date, entry.Status)
-		counters[dkey] = counters[dkey] + entry.Cases
-		countries[entry.Country] = true
-		dates[entry.Date] = true
-	}
+// fileForDate gets the local path of the file for a given date
+func fileForDate(d time.Time) (string) {
+	return fmt.Sprintf("data/%s.csv",  d.Format("01-02-2006"))
+}
 
-	// Sort dates
-	sortedDates := []string{}
-	for k := range dates {
-		sortedDates = append(sortedDates, k)
-	}
-	sort.Strings(sortedDates)
+// urlForDate gets the remote URL of the file for a given date
+func urlForDate(d time.Time) (string) {
+	return fmt.Sprintf(
+		"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/%s.csv",
+		d.Format("01-02-2006"),
+	)
+}
 
-	// Sort countries
-	sortedCountries := []string{}
-	for k := range countries {
-		sortedCountries = append(sortedCountries, k)
-	}
-	sort.Strings(sortedCountries)
+// rowMapper takes a header row and produces a mapping function that will populate a Record with the correct fields
+// This is necessary because the format of the daily reports has changed over time
+func rowMapper(headerRow string) (func(string) Record, error) {
+	headerRow = strings.ToLower(headerRow)
+	headerRow = strings.ReplaceAll(headerRow, "/", "_")
+	headerRow = strings.ReplaceAll(headerRow, "\"", "")
 
-	// Build output records
-	var out []OutputRecord
-	days := map[string]int{}
-	for _, country := range sortedCountries {
-		for _, date := range sortedDates {
+	fields := strings.Split(headerRow, ",")
+	fieldMap := map[string]int{}
+	for index, header := range fields {
+		if regexp.MustCompile(".*country.*").MatchString(header) {
+			fieldMap["country"] = index
+			continue
+		}
 
-			tmp := OutputRecord{
-				Country: country,
-				Date: date,
-				Confirmed: counters[fmt.Sprintf("%s%s%s-day", country, date, "confirmed")],
-				Deaths: counters[fmt.Sprintf("%s%s%s-day", country, date, "deaths")],
-				Recovered: counters[fmt.Sprintf("%s%s%s-day", country, date, "recovered")],
-			}
+		if regexp.MustCompile(".*region|province.*").MatchString(header) {
+			fieldMap["region"] = index
+			continue
+		}
 
-			tmp.Active = tmp.Confirmed - tmp.Recovered - tmp.Deaths
+		if regexp.MustCompile(".*confirm.*").MatchString(header) {
+			fieldMap["confirmed"] = index
+			continue
+		}
 
-			dtDate, _ := time.Parse(time.RFC3339, date)
-			dtDate = dtDate.Add(time.Duration(-24) * time.Hour)
+		if regexp.MustCompile(".*death.*").MatchString(header) {
+			fieldMap["deaths"] = index
+			continue
+		}
 
-			if v, ok := counters[fmt.Sprintf("%s%s%s-day", country, dtDate.Format(time.RFC3339), "confirmed")]; ok {
-				tmp.NewConfirmed = tmp.Confirmed - v
-			}
-
-			if v, ok := counters[fmt.Sprintf("%s%s%s-day", country, dtDate.Format(time.RFC3339), "deaths")]; ok {
-				tmp.NewDeaths = tmp.Deaths - v
-			}
-
-			if v, ok := counters[fmt.Sprintf("%s%s%s-day", country, dtDate.Format(time.RFC3339), "recovered")]; ok {
-				tmp.NewRecovered = tmp.Recovered - v
-			}
-
-			if tmp.Confirmed < 10 {
-				continue
-			}
-
-			// Increment & set days since threshold (10) counter
-			days[tmp.Country] += 1
-			tmp.Day = days[tmp.Country]
-
-			out = append(out, tmp)
+		if regexp.MustCompile(".*recover.*").MatchString(header) {
+			fieldMap["recovered"] = index
+			continue
 		}
 	}
 
-	// Output
-	if os.Args[2] == "csv" {
-		fmt.Printf("Date,Day,Country,Confirmed,Deaths,Recovered,NewConfirmed,NewDeaths,NewRecovered,Active\n")
-		for _, tmp := range out {
-			fmt.Printf("%s,%d,%s,%d,%d,%d,%d,%d,%d,%d\n", tmp.Date, tmp.Day, tmp.Country, tmp.Confirmed, tmp.Deaths, tmp.Recovered, tmp.NewConfirmed, tmp.NewDeaths, tmp.NewRecovered, tmp.Active)
-		}
-	} else if os.Args[2] == "esbulk" {
-		for id, tmp := range out {
-			fmt.Printf("{\"index\": {\"_id\": %d } }\n", id)
-			bytesOut, _ := json.Marshal(tmp)
-			fmt.Println(string(bytesOut))
-		}
-	} else {
-		bytesOut, _ := json.Marshal(out)
-		fmt.Print(string(bytesOut))
+	// Make sure we've got all of the fields
+	if len(fieldMap) != 5 {
+		return nil, fmt.Errorf("missing field mappings: %#v", fieldMap)
 	}
+
+	return func(row string) Record {
+		// We're assuming this data is relatively simple and isn't going to have nested commas
+		row = strings.ReplaceAll(row, "\"", "")
+		row = strings.TrimSpace(row)
+		fields = strings.Split(row, ",")
+		confirmed, _ := strconv.Atoi(fields[fieldMap["confirmed"]])
+		deaths, _ := strconv.Atoi(fields[fieldMap["deaths"]])
+		recovered, _ := strconv.Atoi(fields[fieldMap["recovered"]])
+
+		country := fields[fieldMap["country"]]
+		if country == "Mainland China" {
+			country = "China"
+		}
+
+		return Record {
+			Country: country,
+			Region: fields[fieldMap["region"]],
+			Confirmed: confirmed,
+			Deaths: deaths,
+			Recovered: recovered,
+			Active: confirmed - deaths - recovered,
+			Calculated: make(map[string]int),
+		}
+	}, nil
+}
+
+var counters map[string]int
+var thresholds map[string]*time.Time
+
+func aggregate(r *Record) {
+	if thresholds == nil {
+		thresholds = make(map[string]*time.Time)
+		counters = make(map[string]int)
+	}
+
+	key := r.Country + r.Region + r.Date.Format("2006-02-01")
+
+	counters[key + "confirmed"] += r.Confirmed
+	counters[key + "deaths"] += r.Deaths
+	counters[key + "recovered"] += r.Recovered
+	counters[key + "active"] += r.Active
+
+	thresholds := func(name string, limits []int) {
+		for _, v := range limits {
+			tkey := fmt.Sprintf("%s%d%s", name, v, r.Country)
+			if counters[key + name] > v && thresholds[tkey] == nil {
+				thresholds[tkey] = &r.Date
+			}
+		}
+	}
+
+	thresholds("confirmed", []int{10, 100})
+	thresholds("deaths", []int{10, 100})
+}
+
+func applyAggregates(r *Record) {
+	thresholds := func(key string, name string) {
+		if v := thresholds[key + r.Country]; v != nil {
+			days := int(r.Date.Sub(*v).Hours()/24)
+			if days >= 0 {
+				r.Calculated[name] = days
+			}
+		}
+	}
+	thresholds("confirmed10", "DaysSince10Confirmed")
+	thresholds("confirmed100", "DaysSince100Confirmed")
+	thresholds("deaths10", "DaysSince10Deaths")
+	thresholds("deaths100", "DaysSince100Deaths")
+
+	deltas := func(t string, name string) {
+		yesterday := r.Date.AddDate(0, 0, -1)
+		key := r.Country + r.Region + r.Date.Format("2006-02-01") + t
+		prevKey := r.Country + r.Region + yesterday.Format("2006-02-01") + t
+
+		if _, ok := counters[prevKey]; !ok {
+			r.Calculated[name] = 0
+		} else {
+			r.Calculated[name] = counters[key] - counters[prevKey]
+		}
+	}
+	deltas("confirmed", "ConfirmedDelta")
+	deltas("deaths", "DeathsDelta")
+	deltas("recovered", "RecoveredDelta")
 }
